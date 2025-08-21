58a584fbcfbba6393ac3cba76963b2021e2e7133
Rishi <bingirishitha@gmail.com>
2025-08-21 20:28:17 +0530

    Styling

--- LibraryApp/accounts/urls.py
+++ LibraryApp/accounts/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from . import views
+from .import views
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+
 
 urlpatterns = [
     path("register/", views.register, name="register"),
@@ -9,3 +15,5 @@ urlpatterns = [
     
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
