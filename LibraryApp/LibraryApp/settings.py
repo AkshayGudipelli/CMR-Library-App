c68bce8c1a0d1e0b48314283c78a4c7cb3a32134
AkshayGudipelli <gudipelliakshay18206@gmail.com>
2025-08-22 19:13:10 +0530

    login , register css

--- LibraryApp/LibraryApp/settings.py
+++ LibraryApp/LibraryApp/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/' #it consists of java or css files
+STATIC_URL = '/static/'  #it consists of java or css files
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,"static")
 ]
--- LibraryApp/accounts/forms.py
+++ LibraryApp/accounts/forms.py
@@ -1,18 +1,72 @@
-from django import forms 
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-from django.contrib.auth.forms import UserCreationForm
 
 
 class CollegeUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={
+            'class': 'register-form__input',
+            'placeholder': 'Enter your college email'
+        })
+    )
+
+    password1 = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={
+            'class': 'register-form__input',
+            'placeholder': 'Enter your password'
+        }),
+    )
+
+    password2 = forms.CharField(
+        label="Confirm Password",
+        widget=forms.PasswordInput(attrs={
+            'class': 'register-form__input',
+            'placeholder': 'Confirm your password'
+        }),
+        strip=False,
+    )
+
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
-        
+        fields = ('first_name', 'last_name', 'email', 'password1', 'password2')  # removed username
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'class': 'register-form__input',
+                'placeholder': 'Enter your first name'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'register-form__input',
+                'placeholder': 'Enter your last name'
+            }),
+        }
+
     def clean_email(self):
         email = self.cleaned_data.get('email')
         if not email.endswith('@cmrec.ac.in'):
-            raise ValidationError('You must Register using College Email (ending with @cmrec.ac.in)')
-        return email  
+            raise ValidationError('You must register using College Email (ending with @cmrec.ac.in)')
+        return email
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        # Generate username = firstname+lastname (lowercased, no spaces)
+        first = self.cleaned_data.get('first_name', '').strip().lower()
+        last = self.cleaned_data.get('last_name', '').strip().lower()
+        base_username = f"{first}{last}" if first or last else self.cleaned_data['email'].split('@')[0]
+
+        # Handle duplicates (append number if username exists)
+        username = base_username
+        counter = 1
+        while User.objects.filter(username=username).exists():
+            username = f"{base_username}{counter}"
+            counter += 1
+
+        user.username = username
+
+        if commit:
+            user.save()
+        return user
